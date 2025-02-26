b332637efebe2068612e456004fde906bead3015
HungHuyen113 <152157387+HungHuyen113@users.noreply.github.com>
2025-02-27 01:02:39 +0700

    Add files via upload

---
+++ Models/UpdateInfo.cs
@@ -0,0 +1,21 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class UpdateInfo
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string UpdateAvailable { get; set; }  // "yes" hoặc "no"
+
+    [Required]
+    public string DownloadLink { get; set; }
+
+    [Required]
+    public string UpdateMessage { get; set; }
+
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
